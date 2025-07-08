230a84e314030a8e2e0d39fca177b9df4e88de93
DevFahad <github.com/Fahad4345>
2025-07-08 21:58:20 +0500

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   public/icons/Cart.png 	new file:   public/icons/Globe.png 	new file:   public/icons/Menu.png 	new file:   public/icons/Tree.png 	new file:   public/icons/loading.png 	new file:   public/icons/logo.png 	new file:   public/icons/search.png 	modified:   src/app/globals.css 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,122 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className=" MainTab bg-white ">
+      <div className="Navtab , mx-w-[/1920px]">
+        <div className="firsttab   bg-black justify-between flex  pt-5 pr-7 pb-5 pl-7">
+          <div className="  flex gap-5">
+            <p className="text-[16px] leading-[150%] text-white font-[300px]">
+              See what’s new in Delphi, C++ Builder, and RAD Studio
+            </p>
+            <a href="" className="text-white text-sm">
+              Watch the Replay
+            </a>
+          </div>
+          <div className=" flex  justify-between gap-5">
+            <div className=" flex   gap-5">
+              <a href="http://" className="text-white text-sm">
+                My Downloads
+              </a>
+              <a href="http://" className="text-white text-sm">
+                UpgradeCenter
+              </a>
+              <a href="http://" className="text-white text-sm">
+                MyCustomerPortal
+              </a>
+              <a href="http://" className="text-white text-sm">
+                Contact Us
+              </a>
+            </div>
+            <div className=" flex justify-between gap-5">
+              <Image
+                src="/icons/search.png"
+                alt="My Icon"
+                width={20}
+                height={20}
+              />
+              <Image
+                src="/icons/Cart.png"
+                alt="My Icon"
+                width={20}
+                height={20}
+              />
+              <Image
+                src="/icons/Globe.png"
+                alt="My Icon"
+                width={20}
+                height={20}
+              />
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="secondtab flex  justify-between  px-[10] py-[10]">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/icons/logo.png"
+            alt="My Icon"
+            width={138.67}
+            height={24}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <div className="flex  gap-8">
+            <button className="bg-transparent  text-black  rounded  px-[10px] py-[10px] ">
+              Sign In
+            </button>
+            <button className="rounded border-black text-black border-3 px-[10px] py-[10px]  ">
+              Buy Now
+            </button>
+            <button className="bg-black  text-white  rounded px-[10px] py-[10px] ">
+              {" "}
+              Free Trial
+            </button>
+            <Image src="/icons/Menu.png" alt="My Icon" width={50} height={2} />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between gap-3 px-[150px] ">
+        <div className="  pt-[50px] gap-24">
+          <div className="  gap-33">
+            <button className="bg-white   rounded-[12px]   border-[1px] px-[12px] py-[10px] ">
+              <div className="flex justify-start gap-5">
+                <Image
+                  src="/icons/loading.png"
+                  alt="My Icon"
+                  width={24}
+                  height={24}
+                />
+                <p className="text-black text-sm  font-[500]">
+                  RAD Studio 11.2 out now
+                </p>
+              </div>
+            </button>
+
+            <h1 className="text-black text-[80px] leading-[100%] Tracking-[-2px]">
+              ONE CODEBASE
+            </h1>
+            <h1 className="text-black text-[80px] leading-[100%] Tracking-[-2px]">
+              All PLATFORM
+            </h1>
+          </div>
+          <div className="  pt-[33px]">
+            <div className="  justify-start w-[600px]">
+              <p className="text-black text-[18px] leading-[150%] font-[400] ">
+                Get to market 5x faster with a community full of Technology
+                Partners, MVPs, trainers, authors and developers with the
+                Original Universal Framework for Enterprise App Development
+              </p>
+            </div>
+            <div className="flex pt-[9] gap-2">
+              <button className="bg-black  text-white  rounded px-[10px] py-[10px] ">
+                Free Trial
+              </button>
+              <button className="rounded border-black text-black border-3 px-[10px] py-[10px] ">
+                Buy Now
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className=" py-[50]">
+          <Image src="/icons/Tree.png" alt="My Icon" width={720} height={600} />
+        </div>
+      </div>
     </div>
   );
 }
